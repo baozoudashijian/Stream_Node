78951b266c176b8cfd3652960cfebad85db720bc
Roger Zhang <1787009521@qq.com>
2022-05-04 13:04:40 +0800

    feat: exec执行bash

--- childProcess/14.js
+++ childProcess/14.js
@@ -1,1 +1,12 @@
 // todo 子进程
+const child_process = require('child_process')
+const { exec } = child_process
+
+// ls 默认是项目的根目录
+// ls ../ 
+// ls ./childProcess
+exec('ls ./childProcess', (error, stdout, stderr) => {
+    console.log(error)
+    console.log(stdout)
+    console.log(stderr)
+})
