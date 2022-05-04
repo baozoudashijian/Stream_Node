2f429ee65840546223960ff40615a9e9971d98df
Roger Zhang <1787009521@qq.com>
2022-05-04 14:59:49 +0800

    feat: transform

--- transformExample/23.js
+++ transformExample/23.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
+const { Transform } = require('stream')
 const zlib = require('zlib')
 const file = process.argv[2]
 
+const reportProgress = new Transform({
+    transform(chunk, encoding, callback) {
+        process.stdout.write('-')
+        callback(null, chunk)
+    }
+})
+
 fs.createReadStream(file)
 .pipe(zlib.createGzip())
+.pipe(reportProgress)
 .on('data', () => { process.stdout.write('.') })
 .pipe(fs.createWriteStream(file + '.gz'))
 .on('finish', () => console.log('Done'))
