28694db15ff488222a6d0562ccf4b27494b1e4ab
Roger Zhang <1787009521@qq.com>
2022-05-04 13:42:24 +0800

    feat: spawn

---
+++ childProcess/spawn/19.js
@@ -0,0 +1,11 @@
+// todo 子进程
+const child_process = require('child_process')
+const { spawn } = child_process
+
+const userInput = '.'
+
+const streams = spawn('ls', ['-la', userInput])
+
+streams.stdout.on('data', (chunk) => {
+    console.log(chunk.toString())
+})
